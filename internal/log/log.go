b170294d607e2eecf28c8f94c4a3b052d572257e
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-08-08 18:31:36 +0100

    add color coded logs

--- internal/log/log.go
+++ internal/log/log.go
@@ -2,9 +2,11 @@
 package log
 
 import (
+	"fmt"
 	"log"
 	"time"
 
+	"github.com/NethermindEth/juno/internal/color"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -15,6 +17,7 @@ var Logger *zap.SugaredLogger
 func init() {
 	config := zap.NewDevelopmentConfig()
 	config.DisableStacktrace = true
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	logger, err := config.Build()
 	if err != nil {
 		// notest
@@ -26,6 +29,9 @@ func init() {
 // ReplaceGlobalLogger replace the logger and inject it globally
 func ReplaceGlobalLogger(enableJsonOutput bool, verbosityLevel string) error {
 	config := zap.NewProductionConfig()
+
+	// Colour coding
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 
 	// Timestamp format (ISO8601) and time zone (UTC)
 	config.EncoderConfig.EncodeTime = zapcore.TimeEncoder(func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
@@ -54,5 +60,8 @@ func ReplaceGlobalLogger(enableJsonOutput bool, verbosityLevel string) error {
 	}
 
 	Logger = logger.Sugar()
+	red := color.Red.Add("red")
+	Logger.Info(red, "logger replaced")
+	fmt.Println("\033[1;34mHello World!\033[0m")
 	return nil
 }
